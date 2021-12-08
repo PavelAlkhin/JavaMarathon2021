9021b72c9e251fb9c0a2014fca830ce3f6568f2a
PavelAlkhin <unicon2000@gmail.com>
2021-12-08 17:28:35 +0200

    5 day

--- JavaMarathon2021/src/main/java/day5/Task1.java
+++ JavaMarathon2021/src/main/java/day5/Task1.java
@@ -3,5 +3,13 @@ package day5;
 public class Task1 {
     public static void main(String[] args) {
 
+        Car newCar = new Car();
+        newCar.setModel("Toyota");
+        newCar.setColor("Red");
+        newCar.setYearManufactory(2021);
+        System.out.println(newCar.getModel());
+        System.out.println(newCar.getColor());
+        System.out.println(newCar.getYearManufactory());
     }
 }
+
--- JavaMarathon2021/src/main/java/day5/Task2.java
+++ JavaMarathon2021/src/main/java/day5/Task2.java
@@ -3,6 +3,9 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Motorcycle newMoto = new Motorcycle("Honda", "Red", 2012);
+        System.out.println(newMoto.getModel());
+        System.out.println(newMoto.getColor());
+        System.out.println(newMoto.getYearManufactory());
     }
 }
