94c06fbfb730fa06bee3cc16aaf08321d7c0c9c5
PavelAlkhin <unicon2000@gmail.com>
2021-12-15 12:39:09 +0200

    12 day

--- JavaMarathon2021/src/main/java/day12/task4/Task4.java
+++ JavaMarathon2021/src/main/java/day12/task4/Task4.java
@@ -2,6 +2,11 @@ package day12.task4;
 
 public class Task4 {
     public static void main(String[] args) {
-
+        MusicBand m1 = new MusicBand("Band 1", 1982);
+        m1.addMember("Polit Fartish");
+        m1.addMember("Franklin Yozef");
+        m1.printMembers();
+        MusicBand m2 = new MusicBand("Anothe group", 1983);
+        MusicBand.transferMembers(m1, m2);
     }
 }
--- JavaMarathon2021/src/main/java/day12/task5/Task5.java
+++ JavaMarathon2021/src/main/java/day12/task5/Task5.java
@@ -1,7 +1,14 @@
 package day12.task5;
 
+import day12.task4.MusicBand;
+
 public class Task5 {
     public static void main(String[] args) {
-
+        day12.task4.MusicBand m1 = new day12.task4.MusicBand("Band 1", 1982);
+        m1.addMember("Polit Fartish");
+        m1.addMember("Franklin Yozef");
+        m1.printMembers();
+        day12.task4.MusicBand m2 = new day12.task4.MusicBand("Anothe group", 1983);
+        MusicBand.transferMembers(m1, m2);
     }
 }
