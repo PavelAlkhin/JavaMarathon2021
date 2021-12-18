f3b9fefcd71e1746aa377aba7a519f5c07768436
PavelAlkhin <unicon2000@gmail.com>
2021-12-18 13:28:22 +0200

    15 day

--- JavaMarathon2021/src/main/java/day15/Task1.java
+++ JavaMarathon2021/src/main/java/day15/Task1.java
@@ -1,7 +1,39 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        File file = new File("src/main/resources/shoes.csv");
+        try {
+            Scanner scanner = new Scanner(file);
+            List<Shoe> list = new ArrayList<>();
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] strings = line.split(";");
+                list.add(new Shoe(strings[0], Integer.parseInt(strings[1]), Integer.parseInt(strings[2])));
+            }
+            List<Shoe> shoes = list.stream().filter(shoe -> shoe.getStockBalance() == 0).collect(Collectors.toList());
+
+            PrintWriter pw = new PrintWriter("src/main/resources/missing_shoes.txt");
+            for (Shoe shoe : shoes) {
+                pw.println(shoe.getName() + ";" + shoe.getSize() + ";" + shoe.getStockBalance());
+            }
+            pw.close();
+        }catch (FileNotFoundException e){
+            System.out.println("Не найден файл");
+        }catch (NumberFormatException e){
+            System.out.println("Ошибка преобразования числа");
+        }
+
     }
+
 }
