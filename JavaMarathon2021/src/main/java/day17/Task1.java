6be3d1b37dd1045eba3178954492be7155a6fd2e
PavelAlkhin <unicon2000@gmail.com>
2021-12-20 15:37:44 +0200

    17 day

--- JavaMarathon2021/src/main/java/day17/Task1.java
+++ JavaMarathon2021/src/main/java/day17/Task1.java
@@ -1,7 +1,20 @@
-package day17;
+    package day17;
+
+import java.util.Arrays;
 
 public class Task1 {
     public static void main(String[] args) {
+        ChessPiece[] chessPieces = new ChessPiece[8];
 
+        for (int i = 0; i < 4; i++) {
+            chessPieces[i] = ChessPiece.PAWN_WHITE;
+        }
+        for (int i = 4; i < 8; i++) {
+            chessPieces[i] = ChessPiece.ROOK_BLACK;
+        }
+        for (int i = 0; i < 8; i++) {
+            System.out.print(chessPieces[i].getDesc()+" ");
+
+        }
     }
 }
